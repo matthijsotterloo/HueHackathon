b3e7cbc329c617cce3cf809da544e992f135f368
root <root@cloudmoosic.com>
2015-02-01 04:52:57 +0100

    Automatische Server Commit

---
+++ webapp/register.php
@@ -0,0 +1,20 @@
+<?php
+require_once('auth.inc.php');
+if(!empty($_POST['username']) && !empty($_POST['password'])) {
+	$auth = new Auth();
+	$status = $auth->login($_POST['username'], $_POST['password']);
+	if($status == 'SUCCESS') {
+		header('Location: dashboard.php');
+	}
+}
+?>
+<html>
+<body>
+<h1>Register</h1>
+<form action="register.php" method="POST">
+Username: <input type="text" autocomplete="off" name="username"><br>
+Password: <input type="password" autocomplete="off" name="password">
+<input type="submit">
+</form>
+</body>
+</html>
