28bd75c7b20d3d6cc997c1f9600e5ff7881f7991
root <root@cloudmoosic.com>
2015-02-01 07:31:57 +0100

    Automatische Server Commit

--- image/upload.php
+++ image/upload.php
@@ -4,6 +4,9 @@ header('Access-Control-Allow-Origin: *');
 $data = $_POST['image'];
 
 $data = substr($data, 22, strlen($data));
+
+$data = str_replace(' ', '+', $data);
+
 $data = base64_decode($data);
 
 file_put_contents('image.png', $data);
