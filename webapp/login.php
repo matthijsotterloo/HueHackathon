fb304872757c077da807b86818e9b311d028961d
root <root@cloudmoosic.com>
2015-02-01 04:52:02 +0100

    Automatische Server Commit

--- webapp/login.php
+++ webapp/login.php
@@ -1,6 +1,11 @@
 <?php
+require_once('auth.inc.php');
 if(!empty($_POST['username']) && !empty($_POST['password'])) {
-header('Location: dashboard.php');
+	$auth = new Auth();
+	$status = $auth->login($_POST['username'], $_POST['password']);
+	if($status == 'SUCCESS') {
+		header('Location: dashboard.php');
+	}
 }
 ?>
 <html>
