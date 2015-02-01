50568193bb3d44d29f925879ea27d4e0af822a5b
root <root@cloudmoosic.com>
2015-02-01 07:25:36 +0100

    Automatische Server Commit

--- image/upload.php
+++ image/upload.php
@@ -1,7 +1,7 @@
 <?php
 $data = $_POST['image'];
 
-$data = substr($data, 21, strlen($data));
+$data = substr($data, 22, strlen($data));
 $data = base64_decode($data);
 
 file_put_contents('image.png', $data);
