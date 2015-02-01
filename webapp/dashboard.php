9e5093d64d11da594a3376448529e5825238400e
root <root@cloudmoosic.com>
2015-02-01 05:06:46 +0100

    Automatische Server Commit

--- webapp/dashboard.php
+++ webapp/dashboard.php
@@ -7,14 +7,14 @@ $check = $auth->logincheck();
 if($check != true) {
 	header('Location: login.php?message=You%20have%20to%20log%20in%20first.');
 	die();
-	$userid = $_COOKIE['userid'];
-	echo($userid);
-} else {
-	
 }
 ?>
 <html>
 <body>
-<h1>
+<h1>Dashboard</h1>
+<?php
+$userid = $_COOKIE['userid'];
+echo($userid);
+?>
 </body>
 </html>
